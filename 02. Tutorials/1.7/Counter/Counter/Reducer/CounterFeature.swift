3435d0354f8e593b014af4ec2318585f6355ac66
Cheolhyun Mun <zooxop@gmail.com>
2024-08-19 10:28:21 +0900

    feat: Counter 예제 - Effect 추가

--- 02. Tutorials/1.7/Counter/Counter/Reducer/CounterFeature.swift
+++ 02. Tutorials/1.7/Counter/Counter/Reducer/CounterFeature.swift
@@ -5,6 +5,7 @@
 //  Created by 문철현 on 8/17/24.
 //
 
+import Foundation
 import ComposableArchitecture
 
 @Reducer
@@ -17,14 +18,18 @@ struct CounterFeature {
   struct State {
     var count: Int = 0
     var isTimerOn: Bool = false
+    var fact: String?
+    var isLoading: Bool = false
   }
   
   enum Action: Equatable {
     // 숫자 증감버튼들을 탭할때의 상황
     case incrementButtonTapped
     case decrementButtonTapped
-    case timerToggleTapped(Bool)
+    case timerToggleTapped
     case timerTicked
+    case factButtonTapped
+    case factResponse(String)
   }
   
   private enum CancelID {
@@ -36,18 +41,21 @@ struct CounterFeature {
       switch action {
       case .timerTicked:
         state.count += 1
+        state.fact = nil
         return .none
         
       case .decrementButtonTapped:
         state.count -= 1
+        state.fact = nil
         return .none
         
       case .incrementButtonTapped:
         state.count += 1
+        state.fact = nil
         return .none
         
-      case .timerToggleTapped(let isEnabled):
-        state.isTimerOn = isEnabled
+      case .timerToggleTapped:
+        state.isTimerOn.toggle()
         if state.isTimerOn {
           return .run { send in
             // 주입된 의존성 활용
@@ -60,6 +68,29 @@ struct CounterFeature {
           // Stop the timer
           return .cancel(id: CancelID.timer)
         }
+        
+      case .factButtonTapped:
+        state.fact = nil
+        state.isLoading = true
+        
+        return .run { [count = state.count, isTimerOn = state.isTimerOn] send in
+          if isTimerOn {
+            await send(.timerToggleTapped)
+          }
+          
+          let (data, _) = try await URLSession.shared
+            .data(from: URL(string: "http://numbersapi.com/\(count)")!)
+          
+          let fact = String(decoding: data, as: UTF8.self)
+          await send(.factResponse(fact))
+        }
+        
+      case .factResponse(let fact):
+        state.fact = fact
+        state.isLoading = false
+        
+        return .none
+        
       }
     }
   }
--- 02. Tutorials/1.7/Counter/Counter/View/ContentView.swift
+++ 02. Tutorials/1.7/Counter/Counter/View/ContentView.swift
@@ -14,41 +14,57 @@ struct ContentView: View {
   
   var body: some View {
     WithPerceptionTracking {
-      ZStack {
-        VStack(alignment: .center, spacing: 20.0) {
-          
-          HStack(spacing: 20.0) {
-            counterButton("-") {
-              store.send(.decrementButtonTapped)
-            }
-            
-            
-            Text("\(store.count)")
-              .font(.largeTitle)
-            
-            
-            counterButton("+") {
-              store.send(.incrementButtonTapped)
-            }
-          }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+      VStack(alignment: .center, spacing: 20.0) {
         
-        VStack(alignment: .center) {
-          Toggle(isOn: $store.isTimerOn.sending(\.timerToggleTapped)) {
-            Text("Timer")
+        HStack(spacing: 20.0) {
+          counterButton("-") {
+            store.send(.decrementButtonTapped)
           }
-          .fixedSize()
+          
+          
+          Text("\(store.count)")
+            .font(.largeTitle)
+          
+          
+          counterButton("+") {
+            store.send(.incrementButtonTapped)
+          }
         }
-        .padding(.top, 150)
+        
+        HStack {
+          counterEffectButton("Fact") {
+            store.send(.factButtonTapped)
+          }
+          .background(.blue.opacity(0.8))
+          .clipShape(.capsule)
+          
+          
+          counterEffectButton("Timer") {
+            store.send(.timerToggleTapped)
+          }
+          .background(store.isTimerOn ? .green.opacity(0.8) : .orange.opacity(0.8))
+          .clipShape(.capsule)
+        }
+        
+        HStack {
+          if store.isLoading {
+            ProgressView()
+          } else if let fact = store.fact {
+            Text(fact)
+              .font(.headline)
+              .multilineTextAlignment(.center)
+              .padding()
+          }
+        }
+        .frame(minHeight: 40)
       }
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
   }
 }
 
 extension ContentView {
-  @ViewBuilder
-  private func counterButton(_ title: String, _ action: @escaping () -> ()) -> some View {
+  private func counterButton(_ title: String, _ action: @escaping () -> Void) -> some View {
     Button(title) {
       action()
     }
@@ -56,6 +72,15 @@ extension ContentView {
     .padding()
     .background(.black.opacity(0.1))
     .clipShape(.rect(cornerRadius: 10))
+  }
+  
+  private func counterEffectButton(_ title: String, _ action: @escaping () -> Void) -> some View {
+    Button(title) {
+      action()
+    }
+    .font(.title3)
+    .padding()
+    .foregroundStyle(.white)
   }
 }
 
